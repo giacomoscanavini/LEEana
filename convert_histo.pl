bb5a364106f469f256f316fdb6c9d3e29d7e799c
Xin Qian <last.qx@gmail.com>
2020-10-03 19:27:54 -0400

    update

---
+++ convert_histo.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+open(infile,"./configurations/cv_input.txt");
+
+my $num = 0;
+while(<infile>){
+    @temp = split(/\s+/,$_);
+
+    if ($temp[0] !=-1 && $temp[0] != "\#file"){
+	print "$temp[3]\n";
+	if ($num %12 == 11){
+	    system("./bin/convert_checkout_hist $temp[3] $temp[4]");
+	}else{
+	    system("./bin/convert_checkout_hist $temp[3] $temp[4]&");
+	}
+    }
+    $num ++;
+};
