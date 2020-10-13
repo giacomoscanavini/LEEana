efca1fd6bd044636446598a1b6f4e0b05f4cc020
Xin Qian <last.qx@gmail.com>
2020-10-13 18:38:27 -0400

    update code

--- run_mc_stat.pl
+++ run_mc_stat.pl
@@ -3,9 +3,9 @@
 for (my $i=0;$i != 100; $i++){
     my $lee_strength = 0.03 * $i;
     if ($i % 12 == 11){
-	system("./bin/merge_hist -r1 -e2 -l$lee_strength > ./mc_stat/$i\.log");
+	system("./bin/merge_hist -r0 -e2 -l$lee_strength > ./mc_stat/$i\.log");
     }else{
-	system("./bin/merge_hist -r1 -e2 -l$lee_strength > ./mc_stat/$i\.log &");
+	system("./bin/merge_hist -r0 -e2 -l$lee_strength > ./mc_stat/$i\.log &");
     }
     
 }
