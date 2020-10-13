f6efe0ada5f9a669a5f4771255eabf47abfd6d3c
Xin Qian <last.qx@gmail.com>
2020-10-12 21:26:15 -0400

    update

--- merge_weight.pl
+++ merge_weight.pl
@@ -6,6 +6,9 @@ my $num = 0;
 while(<infile>){
     $filename = $_;
     chomp($filename);
+    @temp = split(/\s+/,$filename);
+    if ($temp[0] eq 'end') last;
+    
     if ($num % 12 == 11){
 	system("./bin/merge_xf $filename");
     }else{
