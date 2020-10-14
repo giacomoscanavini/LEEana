34c97d11564fbef8b535ae47c7419ddaca16aaa5
Xin Qian <last.qx@gmail.com>
2020-10-14 13:57:28 -0400

    update

--- summarize_pot.pl
+++ summarize_pot.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 # $5 and $7 without cuts
 # $8 and $10 for with cuts ...
-system("awk \'\{print \$1,\$2, \$8, \$10\}\' ./pot_counting/potdata/bnb_potdata.txt | grep -v run > pot_bnb.txt");
+system("awk \'\{print \$1,\$2, \$8, \$10\}\' ./pot_counting/data_bnb_potdb.txt | grep -v run > pot_bnb.txt");
 
-system("awk \'\{print \$1,\$2, \$3 \}\' ./pot_counting/potdata/extbnb_potdata.txt | grep -v run > pot_extbnb.txt");
+system("awk \'\{print \$1,\$2, \$3 \}\' ./pot_counting/data_extbnb_potdb.txt | grep -v run > pot_extbnb.txt");
