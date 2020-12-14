b605158e35cd9e96f2bcf355ccb4abfe9ff8aaa0
Xin Qian <last.qx@gmail.com>
2020-12-14 16:30:49 -0500

    update

--- convert_histo.pl
+++ convert_histo.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/perl
 open(infile,"./configurations/cv_input.txt");
 
+my $num1 = scalar(@ARGV);
+print "$num1\n";
 my $num = 0;
 while(<infile>){
     @temp = split(/\s+/,$_);
@@ -12,9 +14,17 @@ while(<infile>){
     if ($temp[0] !=-1 && $temp[0] != "\#file"){
 	print "$temp[3]\n";
 	if ($num %12 == 11){
-	    system("./bin/convert_checkout_hist $temp[3] $temp[4]");
+	    if ($num1 == 0){
+		system("./bin/convert_checkout_hist $temp[3] $temp[4]");
+	    }else{
+		system("./bin/convert_checkout_hist_xs $temp[3] $temp[4]");
+	    }
 	}else{
-	    system("./bin/convert_checkout_hist $temp[3] $temp[4]&");
+	    if ($num1 == 0){
+		system("./bin/convert_checkout_hist $temp[3] $temp[4]&");
+	    }else{
+		system("./bin/convert_checkout_hist_xs $temp[3] $temp[4]&");
+	    }
 	}
     }
     $num ++;
