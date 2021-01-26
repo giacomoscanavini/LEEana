a4cf2237272a810741c0d9af8f067c90067f4ee8
Xin Qian <last.qx@gmail.com>
2021-01-26 14:37:50 -0500

    update

--- plot_kdar.C
+++ plot_kdar.C
@@ -17,34 +17,57 @@ void plot_kdar(){
   T_eval1->AddFriend(T_BDTvars1,"T_BDTvars");
   T_eval1->AddFriend(T_KINEvars1,"T_KINEvars");
 
-  // TCanvas *c1 = new TCanvas("c1","c1",1200,600);
-  // c1->Divide(2,1);
+   TCanvas *c1 = new TCanvas("c1","c1",1200,600);
+   c1->Divide(2,1);
 
-  // c1->cd(1);
+   c1->cd(1);
   TH1F *h1 = new TH1F("h1","h1",50,150,300);
   T_eval->Project("h1","T_KINEvars.kine_reco_Enu","T_BDTvars.numu_score>0.0&&match_isFC==1");
   h1->Draw();
   h1->SetXTitle("E^{rec}_{#nu} (MeV)");
   h1->SetTitle("KDAR #nu (3 MeV per bin)");
   
-  // c1->cd(2);
-  TH1F *h2 = new TH1F("h2","h2",50,150,300);
+   c1->cd(2);
+  TH1F *h2 = new TH1F("h2","h2",50,0,300);
   TH1F *h3 = new TH1F("h3","h3",50,150,300);
-  // T_eval1->Project("h2","truth_nuEnergy"," weight_cv*weight_spline*(1>0)");
-  // T_eval1->Project("h3","truth_nuEnergy"," weight_cv*weight_spline*(T_BDTvars.numu_score >0 &&match_isFC==1)");
-  T_eval1->Project("h2","T_KINEvars.kine_reco_Enu"," weight_cv*weight_spline*(1>0)");
-  T_eval1->Project("h3","T_KINEvars.kine_reco_Enu"," weight_cv*weight_spline*(T_BDTvars.numu_score >0 &&match_isFC==1)");
+  TH1F *h4 = new TH1F("h4","h4",50,150,300);
+  //T_eval1->Project("h2","truth_nuEnergy"," weight_cv*weight_spline*(T_BDTvars.numu_cc_flag >=0 &&match_isFC==1 && truth_vtxInside==1&& truth_isCC==1 && abs(truth_nuPdg)==14)");
+  //T_eval1->Project("h3","truth_nuEnergy"," weight_cv*weight_spline*(T_BDTvars.numu_score >=0 &&match_isFC==1 && truth_vtxInside==1&& truth_isCC==1 && abs(truth_nuPdg)==14)");
+  T_eval1->Project("h2","T_KINEvars.kine_reco_Enu"," weight_cv*weight_spline*(T_BDTvars.numu_score >=0 &&match_isFC==1 && truth_vtxInside==1&& truth_isCC==1 && abs(truth_nuPdg)==14 && truth_nuEnergy>232 && truth_nuEnergy < 240)");
+  // T_eval1->Project("h3","truth_nuEnergy"," weight_cv*weight_spline*(T_BDTvars.numu_cc_flag >=0 &&match_isFC==1 && truth_vtxInside==1&& truth_isCC==1 && abs(truth_nuPdg)==14)");
+  // T_eval1->Project("h4","truth_nuEnergy"," weight_cv*weight_spline*(T_BDTvars.numu_score >=0.9 &&match_isFC==1 && truth_vtxInside==1&& truth_isCC==1 && abs(truth_nuPdg)==14)");
   h2->Draw();
+  h2->SetLineColor(1);
   h3->Draw("same");
   h3->SetLineColor(2);
-  h2->GetYaxis()->SetRangeUser(-20,600);
-  //h2->SetTitle("Truth #nu energy (MeV)");
-  h2->SetTitle("Reco #nu energy (MeV)");
+  h4->Draw("same");
+  h4->SetLineColor(4);
+  //  h2->GetYaxis()->SetRangeUser(-20,600);
+  h2->SetTitle("Truth #nu energy (MeV)");
+  //h2->SetTitle("Reco #nu energy (MeV)");
 
   TLegend *le1 = new TLegend(0.6,0.6,0.89,0.89);
   le1->AddEntry(h2,"all");
-  le1->AddEntry(h3,"Selected");
+  le1->AddEntry(h3,"Generic #nu Det.");
+  le1->AddEntry(h4,"Selected");
   le1->Draw();
 
-  T_eval->Draw("reco_muonMomentum[1]:reco_muonMomentum[2]","T_BDTvars.numu_score >0.0 &&match_isFC==1 && T_KINEvars.kine_reco_Enu>224 && T_KINEvars.kine_reco_Enu<232 && reco_muonMomentum[3]>0.","*");
+  // TH1F *h100 = new TH1F("h100","h100",150,0,1500);
+  // TH1F *h101 = new TH1F("h101","h101",150,0,1500);
+  // TH1F *h102 = new TH1F("h102","h102",150,0,1500);
+  // // T_eval1->Project("h100","truth_energyInside","weight_cv*weight_spline*(truth_vtxInside==1&& truth_isCC==1 && abs(truth_nuPdg)==14)");
+  // //T_eval1->Project("h101","truth_energyInside","weight_cv*weight_spline*(T_BDTvars.numu_cc_flag >=0.0 && truth_vtxInside==1 && truth_isCC==1 && abs(truth_nuPdg)==14)");
+  // T_eval1->Project("h100","truth_nuEnergy","weight_cv*weight_spline*(truth_vtxInside==1&& truth_isCC==1 && abs(truth_nuPdg)==14)");
+  // T_eval1->Project("h101","truth_nuEnergy","weight_cv*weight_spline*(T_BDTvars.numu_cc_flag >=0.0 && truth_vtxInside==1 && truth_isCC==1 && abs(truth_nuPdg)==14)");
+  // T_eval1->Project("h102","truth_nuEnergy","weight_cv*weight_spline*(match_isFC==1 && T_BDTvars.numu_score >=0 && T_BDTvars.numu_cc_flag >=0.0 && truth_vtxInside==1 && truth_isCC==1 && abs(truth_nuPdg)==14)");
+  // h101->Divide(h100);
+  // h102->Divide(h100);
+  
+  // h101->Draw();
+  // h102->SetLineColor(2);
+  // h102->Draw("same");
+  // h101->GetYaxis()->SetRangeUser(0,1.);
+  // le1->Draw();
+  
+  // T_eval->Draw("reco_muonMomentum[1]:reco_muonMomentum[2]","T_BDTvars.numu_score >0.0 &&match_isFC==1 && T_KINEvars.kine_reco_Enu>224 && T_KINEvars.kine_reco_Enu<232 && reco_muonMomentum[3]>0.","*");
 }
--- summarize_pot.pl
+++ summarize_pot.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 # $5 and $7 without cuts
 # $8 and $10 for with cuts ...
-system("awk \'\{print \$1,\$2, \$8, \$10\}\' ./pot_counting/data_bnb_potdb.txt | grep -v run > pot_bnb.txt");
+system("awk \'\{print \$1,\$2, \$8, \$10\}\' ./pot_counting/data_bnb_mcc9.1_wcp*.txt | grep -v run > pot_bnb.txt");
 
-system("awk \'\{print \$1,\$2, \$3 \}\' ./pot_counting/data_extbnb_potdb.txt | grep -v run > pot_extbnb.txt");
+system("awk \'\{print \$1,\$2, \$3 \}\' ./pot_counting/data_extbnb_*.txt | grep -v run > pot_extbnb.txt");
