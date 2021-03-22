b43061280f5b191559b57b81f382823faef11db9
Xin Qian <last.qx@gmail.com>
2021-03-22 18:52:24 -0400

    update code

--- summarize_pot.pl
+++ summarize_pot.pl
@@ -3,4 +3,4 @@
 # $8 and $10 for with cuts ...
 system("cat ./pot_counting/data_bnb_mcc9.1_wcp*.txt | grep 0 | awk \'\{print \$1,\$2, \$8, \$10\}\' | grep -v run > pot_bnb.txt");
 
-system("cat ./pot_counting/data_extbnb_*.txt | grep 0 | awk \'\{print \$1,\$2, \$3 \}\'  | grep -v run > pot_extbnb.txt");
+system("cat ./pot_counting/data_extbnb_*.txt ./pot_counting/ext_bnb_not_found_2021_03_22/run*_potdb.txt | grep 0 | awk \'\{print \$1,\$2, \$3 \}\'  | grep -v run > pot_extbnb.txt");
