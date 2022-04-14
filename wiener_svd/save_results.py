0ca9d8fb3a1d4b194f4a17052c4a2a1a00e9017b
giacomo <gs627@DESKTOP-DDS57IK.localdomain>
2022-04-14 16:58:47 -0400

    Added info such that differential cross section is now possible

--- wiener_svd/save_results.py
+++ wiener_svd/save_results.py
@@ -7,3 +7,8 @@ target = sys.argv[1]
 os.system('mkdir %s'%(target))
 os.system('cp output.root wiener.root merge_xs.root ./%s'%(target))
 os.system('cp -rf ./DetVar ./XsFlux ./mcstat ./hist_rootfiles_xs ./%s'%(target))
+
+os.system('cp ../configurations/cov_input.txt ./%s'%(target))
+os.system('cp ../configurations/xs_ch.txt ./%s'%(target))
+os.system('cp ../configurations/xs_real_bin.txt ./%s'%(target))
+os.system('cp ../configurations/cv_input.txt ./%s'%(target))
