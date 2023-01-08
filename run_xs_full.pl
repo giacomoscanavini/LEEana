d14ab6ac11367296a55c529844edfc99a2ca9405
giacomo <gs627@DESKTOP-DDS57IK.localdomain>
2023-01-08 01:27:01 -0500

    New Cross Sections

--- run_xs_full.pl
+++ run_xs_full.pl
@@ -1,22 +1,65 @@
 #!/usr/bin/perl
 
+my $num1 = scalar(@ARGV);
+if ($num1 > -1){
+    $num1 = $ARGV[0];
+    print "$num1\n";
+}
+# if num1 == 0: xs_total_Xp
+# if num1 == 1: xs_total_0pNp
+# if num1 == 2: xs_diff_cos_Xp
+# if num1 == 3: xs_diff_cos_0pNp
+# if num1 == 4: xs_diff_mom_Xp
+# if num1 == 5: xs_diff_mom_0pNp
+# if num1 == 6: xs_2D_Xp
 
 system("rm ./configurations/file_ch.txt");
-system("rm ./configurations/cov_input.txt");
 system("rm ./configurations/cv_input.txt");
 system("rm ./configurations/det_file_ch.txt");
 system("rm ./configurations/det_input.txt");
 system("rm ./configurations/xf_file_ch.txt");
 system("rm ./configurations/xf_input.txt");
+system("rm ./configurations/cov_input.txt");
+system("rm ./configurations/xs_ch.txt");
+system("rm ./configurations/xs_real_bin.txt");
 
+# They are all the same for all cross sections
 system("cp ./configurations/file_ch_xs_full.txt ./configurations/file_ch.txt");
-system("cp ./configurations/cov_input_xs_full.txt ./configurations/cov_input.txt");
 system("cp ./configurations/cv_input_full.txt ./configurations/cv_input.txt");
 system("cp ./configurations/det_file_ch_full.txt ./configurations/det_file_ch.txt");
 system("cp ./configurations/det_input_full.txt ./configurations/det_input.txt");
 system("cp ./configurations/xf_file_ch_full.txt ./configurations/xf_file_ch.txt");
 system("cp ./configurations/xf_input_full.txt ./configurations/xf_input.txt");
-
+# They change based on the cross section
+if ($num1 == 0){
+	system("cp ./configurations/xs_total_Xp/cov_input.txt ./configurations/cov_input.txt");
+	system("cp ./configurations/xs_total_Xp/xs_ch.txt ./configurations/xs_ch.txt");
+	system("cp ./configurations/xs_total_Xp/xs_real_bin.txt ./configurations/xs_real_bin.txt");
+}elsif ($num1 == 1){
+	system("cp ./configurations/xs_total_0pNp/cov_input.txt ./configurations/cov_input.txt");
+	system("cp ./configurations/xs_total_0pNp/xs_ch.txt ./configurations/xs_ch.txt");
+	system("cp ./configurations/xs_total_0pNp/xs_real_bin.txt ./configurations/xs_real_bin.txt");
+}elsif ($num1 == 2){
+	system("cp ./configurations/xs_diff_cos_Xp/cov_input.txt ./configurations/cov_input.txt");
+	system("cp ./configurations/xs_diff_cos_Xp/xs_ch.txt ./configurations/xs_ch.txt");
+	system("cp ./configurations/xs_diff_cos_Xp/xs_real_bin.txt ./configurations/xs_real_bin.txt");
+}elsif ($num1 == 3){
+	system("cp ./configurations/xs_diff_cos_0pNp/cov_input.txt ./configurations/cov_input.txt");
+	system("cp ./configurations/xs_diff_cos_0pNp/xs_ch.txt ./configurations/xs_ch.txt");
+	system("cp ./configurations/xs_diff_cos_0pNp/xs_real_bin.txt ./configurations/xs_real_bin.txt");
+}elsif ($num1 == 4){
+	system("cp ./configurations/xs_diff_mom_Xp/cov_input.txt ./configurations/cov_input.txt");
+	system("cp ./configurations/xs_diff_mom_Xp/xs_ch.txt ./configurations/xs_ch.txt");
+	system("cp ./configurations/xs_diff_mom_Xp/xs_real_bin.txt ./configurations/xs_real_bin.txt");
+}elsif ($num1 == 5){
+	system("cp ./configurations/xs_diff_mom_0pNp/cov_input.txt ./configurations/cov_input.txt");
+	system("cp ./configurations/xs_diff_mom_0pNp/xs_ch.txt ./configurations/xs_ch.txt");
+	system("cp ./configurations/xs_diff_mom_0pNp/xs_real_bin.txt ./configurations/xs_real_bin.txt");
+}elsif ($num1 == 6){
+	system("cp ./configurations/xs_2D_Xp/cov_input.txt ./configurations/cov_input.txt");
+	system("cp ./configurations/xs_2D_Xp/xs_ch.txt ./configurations/xs_ch.txt");
+	system("cp ./configurations/xs_2D_Xp/xs_real_bin.txt ./configurations/xs_real_bin.txt");
+}
 
 #central histogram preparation ...// Xs mode ...
 system("./convert_histo.pl 1");
